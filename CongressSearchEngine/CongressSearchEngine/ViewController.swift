53f66dda08a54001530625e32091eeb0382a0aa4
Elliscope Fang <mingzhef@usc.edu>
2016-11-27 21:42:58 -0800

    starting the project

---
+++ CongressSearchEngine/CongressSearchEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CongressSearchEngine
+//
+//  Created by Elliscope Mingzhe Fang on 11/27/16.
+//  Copyright © 2016 Elliscope Mingzhe Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
